051102d3dcc13899464246c81aba18648880bb97
Mohammed Maher Abdelrahim Mohammed <maherali8932@gmail.com>
2022-05-30 16:01:14 +0200

    simplify

--- examples/latex_test.cpp
+++ examples/latex_test.cpp
@@ -52,7 +52,11 @@ int main()
         Brapsi_phi_Ket = psi_Bra*phi_Ket;
         
 	LaTex mypdf("output/latex_results.tex");
+	
+	
 	mypdf.BeginLaTex();
+	//mypdf.ToLaTex("k",psi_Ket);
+	
 	mypdf.MathOperation("tktbto","\\ket{\\psi} = ",psi_Ket,", \\bra{\\phi} = ",phi_Bra,", A = ",A);
 	mypdf.MathOperation("tok=k","A\\ket{\\psi} = ",A,psi_Ket,'=',A_TIMS_psi_Ket);
 	mypdf.MathOperation("tbok=n","\\bra{\\phi}A\\ket{\\psi}= ",phi_Bra,A,psi_Ket,'=',AOA);
@@ -60,6 +64,7 @@ int main()
 	mypdf.MathOperation("tbk=n","\\braket{\\phi|\\psi}= ",phi_Bra,psi_Ket,'=',Braphi_KetA);
 	mypdf.MathOperation("tbk=n","\\braket{\\psi|\\psi}= ",psi_Bra,psi_Ket,'=',Brapsi_KetA);
 	mypdf.MathOperation("tbk=n","\\braket{\\phi|\\phi}= ",phi_Bra,phi_Ket,'=',Braphi_KetB);
+	
 	mypdf.EndLaTex();
 	
 	
--- include/latex.h
+++ include/latex.h
@@ -1,25 +1,54 @@
-/*******************************************************************************************
+/*=================================================================================
  generate LaTex files for results output 
 
 by:  Mohammed Maher Abdelrahim Mohammed
      UNIVERSITÀ DELLA CALABRIA, DIPARTIMENTO DI FISICA AND INFN-COSENZA
      VIA P. BUCCI, CUBO 31 C, I-87036 COSENZA, ITALY
      mohammed.maher@unical.it                                          
-*******************************************************************************************/
+=================================================================================*/
 //#pragma once
 #ifndef LATEX_H
 #define LATEX_H
 #include<iostream>
 #include<cmath>
+#include"utilities.h"
 
+ 
+/*********************************************************************************
+  Class:    LaTex.
 
+  Summary:  Ket is a public derived from the well designed Eigen::Matrix class. 
+	    Ket vectors represent matrices with one column and Dynamic-size rows,
+
+  Methods:  BeginLaTex()
+              set and initiate the LaTex typesetting file document class.
+            
+            EndLaTex()
+              end the LaTex typesetting file document class.
+            
+            Typing()
+              allows to directly write normal text or even latex commands 
+            
+            ToLaTex()
+              transform multiple input into latex format.
+            
+            MathOperation()
+              transform multiple mathematical operations into latex equations.
+            
+            LaTex()
+              Constructor.
+            
+            LaTex(std::string filename)
+              Copy Constructor.
+            
+            ~LaTex()
+              Destructor.
+********************************************************************************/
 class LaTex {
 	private:
 		std::ofstream ofile;
 		std::string file_path;
-
-	public: 
-				
+	public: 				
 		LaTex(): file_path("output/default.tex") {}
 		LaTex(std::string filename): file_path(filename) {}	
 		
@@ -51,120 +80,111 @@ class LaTex {
 			ofile<<text;
 			ofile.close();
 		}
+		
 		void ToLaTex(const char* fmt...);
 		void MathOperation(const char* fmt...);
 };
-//=================================================================================
-//--------------------------------------------------------------------------
-// convert LaTex output
-//--------------------------------------------------------------------------
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Function: RealAndImaginary
+  
+  Summary:   convert tow double variables into string format, with 
+  	     chosen separator string. 
+             
+  Returns:  string
+             store the output directly into string variable.
+-------------------------------------------------------------------*/
+std::string RealAndImaginary(double x, double y, std::string separator)
+{
+	std::string resulted_string;
+	if(x!=0 and y!=0) {
+		
+		if(y<0) {		
+			resulted_string = ToString(x)+ToString(y)+"i"+separator;
+		}
+
+		else if(y>0) {
+			resulted_string = ToString(x)+"+"+ToString(y)+"i"+separator;
+		}
+	}
+	else if(y==0) {   
+		resulted_string = ToString(x)+separator; 
+	}
+	else if(x==0){
+		resulted_string = ToString(y)+"i"+separator; 
+	}
+	return  resulted_string;
+}
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Function: ToLaTex
+  
+  Summary:   convert multiple inputs into latex format.
+  
+  Args:     const char* fmt...
+              multiple variables with identifier format "fmt".
+             
+  Returns:  void
+              print the output directly to the specified latex file
+-------------------------------------------------------------------*/
 void LaTex::ToLaTex(const char* fmt...)
 {	
 	va_list args;
 	va_start(args, fmt);
  
-	ofile.open(file_path,std::ios_base::app);
+	ofile.open(file_path,std::ios_base::app);  ///<--- opern the targeted file and appending its contents 
  
-	ofile<<"\\begin{align}\n";
-	while (*fmt != '\0') {       
-		if (*fmt == 'k') {
+	ofile<<"\\begin{align}\n";    ///<-------- latex command to start an equation 
+	while (*fmt != '\0') {       ///<-- while there is identifier char: detect its indicating case
+		if (*fmt == 'k') { ///<--- ket case indicator 
 			
 			Ket<std::complex<double>> mt = va_arg(args, Ket<std::complex<double>>);
 
 	    		ofile<<"\\begin{pmatrix}\n";
 			for(int i =0; i<mt.rows(); i++)
 		    	{
-				for(int j =0; j<mt.cols(); j++){
-				 
+				for(int j =0; j<mt.cols(); j++)
+				{ 
 					double x = std::real(mt(i,j));
-					double y = std::imag(mt(i,j));   
-					if(x!=0 and y!=0) {
-						
-						if(y<0) {		
-			    				ofile<<"$"<<x<<y<<"i$"<<"\\\\";
-						}
-
-						else if(y>0) {
-			    				ofile<<"$"<<x<<"+"<<y<<"i$"<<"\\\\";
-						}
-					}
-		   			else if(y==0) {   
-						ofile<<"$"<<x<<"$"<<"\\\\";
-		    			}
-		    			else if(x==0){
-						ofile<<"$"<<y<<"i$"<<"\\\\";
-		    			}   
+					double y = std::imag(mt(i,j)); 
+					ofile<<RealAndImaginary(x,y,"\\\\");   
 		    		}
 		    	}
 			 	ofile<<"\\end{pmatrix}";
-		  		ofile<<"\n";
 		}
-		else if (*fmt == 'b') {
+		else if (*fmt == 'b') { ///<--- bra case indicator
 			
 			Bra<std::complex<double>> mt = va_arg(args, Bra<std::complex<double>>);
 
 			ofile<<"\\begin{pmatrix}\n";
 			for(int i =0; i<mt.rows(); i++)
 		    	{
-				for(int j =0; j<mt.cols(); j++) {
-				 
+				for(int j =0; j<mt.cols(); j++)
+				{
 					double x = std::real(mt(i,j));
-					double y = std::imag(mt(i,j));   
-					if(x!=0 and y!=0) {
-					
-						if(y<0) {
-				    			ofile<<x<<y<<"i\\quad";
-						}
-
-						else if(y>0) {
-				    			ofile<<x<<"+"<<y<<"i\\quad";	 
-						}
-					}
-			   		else if(y==0) {   
-						ofile<<x<<"\\quad";	 
-			    		}
-			    		else if(x==0) {
-						ofile<<y<<"i\\quad";	 
-			    		}	   		    		
+					double y = std::imag(mt(i,j));
+					ofile<<RealAndImaginary(x,y,"\\quad");   	   		    		
 		    		}
 		    	
 		    	}
-		    	ofile<<"\\end{pmatrix}";
+		    	ofile<<"\\end{pmatrix}"; 
 	    	}
-		else if (*fmt == 'o') {
+		else if (*fmt == 'o') { ///<--- operator case indicator
 			
 			QM_operator<std::complex<double>> mt = va_arg(args, QM_operator<std::complex<double>>);
  
 			ofile<<"\\begin{pmatrix}\n";
 			for(int i =0; i<mt.rows(); i++)
 		    	{
-				for(int j =0; j<mt.cols(); j++) {
-				
+				for(int j =0; j<mt.cols(); j++)
+				{				
 					double x = std::real(mt(i,j));
-					double y = std::imag(mt(i,j));   
-					if(x!=0 and y!=0) {
-					
-						if(y<0) {
-				    			ofile<<x<<y<<"i &";
-						}
-
-						else if(y>0) {
-				    			ofile<<x<<"+"<<y<<"i &";
-						}
-					}
-			   		else if(y==0) {   
-						ofile<<x<<"&";
-		 
-			    		}
-			    		else if(x==0) {
-						ofile<<y<<"i &";
-			    		}	   
+					double y = std::imag(mt(i,j));
+					ofile<<RealAndImaginary(x,y," &");   	   
 		    		}
 		    		ofile<<"\\\\";
 		    	} 
 		    	ofile<<"\\end{pmatrix}";
 	    	}
-	    	else if (*fmt == 't') {
+	    	else if (*fmt == 't') { ///<--- text case indicator
 	    		char* s = va_arg( args, char * );
             		ofile << s;
 	    	}
@@ -172,13 +192,21 @@ void LaTex::ToLaTex(const char* fmt...)
 	}
  	va_end(args);  	
 	ofile<<"\n";
-	ofile<<"\\end{align}\n";
+	ofile<<"\\end{align}\n"; ///<-------- latex command to end an equation 
 	ofile.close();
 }
 
-//--------------------------------------------------------------------------
-// convert LaTex output operations
-//--------------------------------------------------------------------------
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Function: MathOperation
+  
+  Summary:   convert multiple mathematical operations into latex equations.
+  
+  Args:     const char* fmt...
+              multiple variables with identifier format "fmt".
+             
+  Returns:  void
+              print the output directly to the specified latex file
+---------------------------------------------------------------------------------------*/
 void LaTex::MathOperation(const char* fmt...)
 {	
 	va_list args;
@@ -188,135 +216,70 @@ void LaTex::MathOperation(const char* fmt...)
 	
 	ofile<<"\\begin{align}\n";
 	while (*fmt != '\0') {       
-		if (*fmt == 'k') {
+		if (*fmt == 'k') { ///<--- ket case indicator
 			
 			Ket<std::complex<double>> mt = va_arg(args, Ket<std::complex<double>>);
 			
 	    		ofile<<"\\begin{pmatrix}\n";
 			for(int i =0; i<mt.rows(); i++)
 		    	{
-				for(int j =0; j<mt.cols(); j++){
-				 
+				for(int j =0; j<mt.cols(); j++) 
+				{				 
 					double x = std::real(mt(i,j));
-					double y = std::imag(mt(i,j));   
-					if(x!=0 and y!=0) {
-						
-						if(y<0) {		
-			    				ofile<<"$"<<x<<y<<"i$"<<"\\\\";
-						}
-
-						else if(y>0) {
-			    				ofile<<"$"<<x<<"+"<<y<<"i$"<<"\\\\";
-						}
-					}
-		   			else if(y==0) {   
-						ofile<<"$"<<x<<"$"<<"\\\\";
-		    			}
-		    			else if(x==0){
-						ofile<<"$"<<y<<"i$"<<"\\\\";
-		    			}   
+					double y = std::imag(mt(i,j)); 
+					ofile<<RealAndImaginary(x,y,"\\\\");     
 		    			ofile<<"\n";
 		    		}
 		    	}
-			 	ofile<<"\\end{pmatrix}";
-		  		 
+			 	ofile<<"\\end{pmatrix}";	 
 		}
-		else if (*fmt == 'b') {
+		else if (*fmt == 'b') {///<--- bra case indicator
 			
 			Bra<std::complex<double>> mt = va_arg(args, Bra<std::complex<double>>);
- 
-			//ofile<<"[\\,\\quad";
 			ofile<<"\\begin{pmatrix}\n";
+			
 			for(int i =0; i<mt.rows(); i++)
 		    	{
-				for(int j =0; j<mt.cols(); j++) {
-				 
+				for(int j =0; j<mt.cols(); j++) 
+				{
 					double x = std::real(mt(i,j));
-					double y = std::imag(mt(i,j));   
-					if(x!=0 and y!=0) {
-					
-						if(y<0) {
-				    			ofile<<x<<y<<"i\\quad";
-						}
-
-						else if(y>0) {
-				    			ofile<<x<<"+"<<y<<"i\\quad";	 
-						}
-					}
-			   		else if(y==0) {   
-						ofile<<x<<"\\quad";	 
-			    		}
-			    		else if(x==0) {
-						ofile<<y<<"i\\quad";	 
-			    		}	   		    		
+					double y = std::imag(mt(i,j));
+					ofile<<RealAndImaginary(x,y,"\\quad");  	   		    		
 		    		}
 		    	
 		    	}
-		    	//ofile<<"\\,]";
 		    	ofile<<"\\end{pmatrix}";
 	    	}
-		else if (*fmt == 'o') {
+		else if (*fmt == 'o') { ///<--- operator case indicator
 			
 			QM_operator<std::complex<double>> mt = va_arg(args, QM_operator<std::complex<double>>);
 			ofile<<"\\begin{pmatrix}\n";
 			for(int i =0; i<mt.rows(); i++)
 		    	{
-				for(int j =0; j<mt.cols(); j++) {
-				
+				for(int j =0; j<mt.cols(); j++) 
+				{
 					double x = std::real(mt(i,j));
-					double y = std::imag(mt(i,j));   
-					if(x!=0 and y!=0) {
-					
-						if(y<0) {
-				    			ofile<<x<<y<<"i &";
-						}
-
-						else if(y>0) {
-				    			ofile<<x<<"+"<<y<<"i &";
-						}
-					}
-			   		else if(y==0) {   
-						ofile<<x<<"&";
-		 
-			    		}
-			    		else if(x==0) {
-						ofile<<y<<"i &";
-			    		}	   
+					double y = std::imag(mt(i,j));
+					ofile<<RealAndImaginary(x,y," &");  
 		    		}
 		    		ofile<<"\\\\";
 		    	} 
 		    	ofile<<"\\end{pmatrix}";
 		    	
 	    	}
-	    	else if (*fmt == 'n') {
+	    	else if (*fmt == 'n') { ///<--- complex number case indicator
 			std::complex<double> c = va_arg(args, std::complex<double>);
     			double x = std::real(c);
-			double y = std::imag(c);   
-			if(x!=0 and y!=0) {
-			
-				if(y<0) {
-		    			ofile<<x<<y<<"i &";
-				}
-
-				else if(y>0) {
-		    			ofile<<x<<"+"<<y<<"i &";
-				}
-			}
-	   		else if(y==0) {   
-				ofile<<x<<"&";
- 
-	    		}
-	    		else if(x==0) {
-				ofile<<y<<"i &";
-	    		}
+			double y = std::imag(c); 
+			ofile<<RealAndImaginary(x,y," &");  
 	    	}
 	    	
 	    	
-	    	else if (*fmt == '=') {
+	    	else if (*fmt == '=') { ///<--- equal sign case indicator
     			int c = va_arg(args, int);
             		ofile << static_cast<char>(c);
 	    	}
-	    		else if (*fmt == 't') {
+	    		else if (*fmt == 't') { ///<--- text case indicator
 	    		char* s = va_arg( args, char * );
             		ofile << s;
 	    	}
