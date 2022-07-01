4da3e76a583862124df5dd3928a70e81554212bc
Mohammed Maher Abdelrahim Mohammed <maherali8932@gmail.com>
2022-07-01 21:40:10 +0200

    .

--- include/timer.h
+++ include/timer.h
@@ -9,8 +9,11 @@ class ElapsedTime{
 		void Start(){
 			start = std::clock();
 		}
-		void End(){ std::cout<<"------------------------------------------"<<std::endl;
+		void End()
+		{
+			std::cout<<"------------------------------------------"<<std::endl;
 			duration = static_cast<double>(std::clock() - start) /static_cast<double> CLOCKS_PER_SEC;
+			
 			if (duration < 60.0) {
 				std::cout << "Elapsed time: " << duration << " seconds !!" << std::endl;
 			}
